e9198c00b68a2ed091cf87c15f857f841efa3c70
Tran Van Binh <tranvanbinh150105@gmail.com>
2026-02-03 14:34:18 +0700

    hoan thanh bai tap 29

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio đã sẵn sàng!');
+    
+    // Hiển thị lời chào khi khách truy cập
+    setTimeout(() => {
+        alert('Chào mừng bạn đến với Portfolio của mình trên GitHub Pages!');
+    }, 500);
+});
